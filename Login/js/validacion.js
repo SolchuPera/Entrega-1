6728bc7bc282a3b8791274f4b89a0ad96c240bac
Lucía <luciagastelu@gmail.com>
2024-08-20 19:37:41 -0300

    Update validacion.js
    Chequear que los campos no estan vacios y redirige si no lo estan.

--- Login/js/validacion.js
+++ Login/js/validacion.js
@@ -5,3 +5,20 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+function validarForm(){
+    const username = document.getElementById("usuario").value;
+    const password = document.getElementById("contraseña").value;
+    // Revisa que los campos no estan vacios
+    if (username === "" || password === "") {
+        //Mostrar el error
+        showAlertError();
+    } else {
+        // No se si index_2 es la portada así que le voy a dejar un nombre autodescriptivo
+        showAlertSuccess();
+        setTimeout(() => {
+            window.location.href = "portada.html";
+        }, 10000); 
+        //10000 hace que espere 10 segundos así ve el alertsuccess sino lo saco 
+        // Lo de windows location etc en lugar de href lo obtuve del punto 4 de la entrega
+    }
+}
